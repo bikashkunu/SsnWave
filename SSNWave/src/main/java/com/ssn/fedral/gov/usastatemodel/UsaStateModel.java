7ae323a50269f97a7550b3b3ad7c3dda27915aa8
bikashkunu <49331598+bikashkunu@users.noreply.github.com>
2019-09-28 09:35:32 +0530

    b

---
+++ SSNWave/src/main/java/com/ssn/fedral/gov/usastatemodel/UsaStateModel.java
@@ -0,0 +1,12 @@
+package com.ssn.fedral.gov.usastatemodel;
+
+import javax.persistence.Column;
+
+import lombok.Data;
+
+@Data
+public class UsaStateModel {
+	private int stateCode;
+	private String STATENAME;
+
+}
